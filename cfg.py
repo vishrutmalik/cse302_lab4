868cc420e669a1919281a51df3cffbc3a89d5c88
Vishrut Malik <vishrutmalik21@gmail.com>
2021-10-11 22:22:19 +0200

    body

--- cfg.py
+++ cfg.py
@@ -1,1 +1,26 @@
 import sys
+
+
+class cfg:
+    def __init__(self, js_obj):
+        self.obj=js_obj
+        self.edges={}
+        pass
+
+    def new_node(self):
+        pass
+
+
+class Node(cfg):
+    def __init__(self, pred, succ):
+        self.pred= None; #as of now
+        self.succ= None; #as of now
+        pass
+    
+    def next(self):
+        return self.succ
+
+    def prev(self):
+        return self.pred
+
+
