7e583a3adf9a875265d41030dff60b7760d00eb3
Vishrut Malik <vishrutmalik21@gmail.com>
2021-10-12 20:10:54 +0200

    class skeleton

--- cfg.py
+++ cfg.py
@@ -1,18 +1,9 @@
 import sys
 
-
-class cfg:
-    def __init__(self, js_obj):
-        self.obj=js_obj
-        self.edges={}
-        pass
-
-    def new_node(self):
-        pass
-
-
-class Node(cfg):
-    def __init__(self, pred, succ):
+class Node:
+    def __init__(self, pred, succ, body):
+        self.label=None
+        self.body=body
         self.pred= None; #as of now
         self.succ= None; #as of now
         pass
@@ -22,5 +13,42 @@ class Node(cfg):
 
     def prev(self):
         return self.pred
+
+    def instr(self):
+        pass
+
+class CFG:
+    def __init__(self, js_obj, proc, label, nodes):
+        self.obj=js_obj
+        self.proc= proc
+        self.label=label
+        pass
+
+    def new_node(self):
+        pass
+
+    def successor(self):
+        pass
+
+    def predecessor(self):
+        pass
+
+    def edges(self):
+        #create a tuples?
+        pass
+
+    def new_node(self):
+        pass
+
+    def delete_node(self):
+        pass
+    
+    def remove_edge(self):
+        pass
+
+    def add_edge(self):
+        pass
+
+    
 
 
