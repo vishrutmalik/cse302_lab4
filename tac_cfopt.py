ea43b02ab39fd0b075d728cff0e1ddada52a3e53
remyjck <remy.seassau@polytechnique.edu>
2021-10-12 20:56:37 +0200

    Added argument parser and updated gitignore

--- tac_cfopt.py
+++ tac_cfopt.py
@@ -1,4 +1,15 @@
 import json
 import sys
+import argparse
 from cfg import *
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fname")
+    parser.add_argument("-o")
+    parser.add_argument("--disable-coal", action="store_true", required=False)
+    parser.add_argument("--disable-uce", action="store_true", required=False)
+    parser.add_argument("--disable-jp1", action="store_true", required=False)
+    parser.add_argument("--disable-jp2", action="store_true", required=False)
+    args = parser.parse_args()
+    print(args)
