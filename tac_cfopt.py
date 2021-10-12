7e0c2d4014fc84fd04201b8cdc3c42f94a497c5d
remyjck <remy.seassau@polytechnique.edu>
2021-10-12 21:24:18 +0200

    Started beginning half of block inference

--- tac_cfopt.py
+++ tac_cfopt.py
@@ -4,8 +4,37 @@ import argparse
 from cfg import *
 
 
+def get_labels(body):
+    labels = set()
+    for instr in body:
+        if instr["opcode"] == "label":
+            assert instr["args"][0][:1] == "%."
+            labels.add(instr["args"][0])
+
+    return labels
+
+def new_label(labels, label_counter):
+    while f"%.L{label_counter}" in labels:
+        label_counter += 1
+    return (f"%.L{label_counter}", label_counter)
+
+def add_labels_jumps(proc):
+    assert proc["proc"][0] == '@'
+    assert proc["body"][0]["args"][0] == '@'
+    body = proc["body"]
+    labels = get_labels(body)
+    label_counter = len(labels) #Could be 0 but this might save time
+    if body[1]["opcode"] != "label":
+        new_lbl, label_counter = new_label(labels, label_counter)
+        body.insert({"opcode":"label", "args":[new_lbl], "result":None})
+
+
 def main(fname, sname, coal, uce, jp1, jp2):
-    pass
+    with open(fname, 'r') as f:
+        js_obj = f.read()
+
+    for proc in js_obj:
+        add_labels_jumps(proc)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -17,5 +46,5 @@ if __name__ == "__main__":
     parser.add_argument("--disable-jp2", action="store_true", required=False)
     args = parser.parse_args()
 
-    main(args.fname, args.o, args.disable-coal, args.disable-uce,
-         args.disable-jp1, args.disable-jp2)
+    main(args.fname, args.o, args.disable_coal, args.disable_uce,
+         args.disable_jp1, args.disable_jp2)
