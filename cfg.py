bbc21954e792d61be6f9ff1b32bd4bf08f633938
Remy Seassau <r.seassau@eleves-alsacienne.org>
2021-10-14 03:50:11 +0200

    Added uce (untested)

--- cfg.py
+++ cfg.py
@@ -16,10 +16,12 @@ class Node:
             if instr["opcode"][0]=='j' and instr["args"][-1] not in self.jumps:
                 self.jumps.append(instr["args"][-1])
 
+
 class CFG:
     def __init__(self,proc,nodes):
         self.proc= proc
         self.nodes=nodes
+        self.entry = self.nodes[1]
         self.edges=dict()
         self.update_edges()
 
@@ -44,13 +46,11 @@ class CFG:
             self.nodes.append(node)
             self.update_edges()
 
-
     def delete_node(self, node):
         if node in self.nodes:
             for dest in self.edges[node.label]:
                 self.remove_edge(node.label,dest)
             self.nodes.remove(node)
-        
     
     def remove_edge(self,src,dest):
         if dest in self.edges[src]:
@@ -59,7 +59,17 @@ class CFG:
     def add_edge(self,src,dest):
         if dest not in self.edges[src]:
             self.edges[src].append(dest)
+    
+    def aux_uce(self, node, visited):
+        visited.add(node)
+        for neighbour in self.next(node):
+            if neighbour not in visited:
+                self.uce_aux(neighbour, visited)
 
-
-
-
+    def uce(self):
+        visited = set()
+        self.aux_uce(self.entry, visited)
+        for node in self.nodes:
+            if node not in visited:
+                self.delete_node(node)
+        self.update_edges()
