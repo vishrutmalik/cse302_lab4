ea12d9216d99e6ec317e43da49ee5526afb2e699
remyjck <remy.seassau@polytechnique.edu>
2021-11-08 20:20:02 +0100

    Fixed range to allow negative numbers

--- scanner.py
+++ scanner.py
@@ -84,8 +84,8 @@ def t_NUMBER(t):
     r'0|-?[1-9][0-9]*'
     # t.type == 'NUMBER'
     t.value = int(t.value)
-    if not (0 <= t.value < (1<<63)):
-        print(t, f'Error: illegal numerical literal, {t.value} not in range(0, 1<<63) on line {t.lineno}')
+    if not (-(1<<63) <= t.value < (1<<63)):
+        print(t, f'Error: illegal numerical literal, {t.value} not in range(-1<<63, 1<<63) on line {t.lineno}')
         sys.exit(1)
     return t
 
